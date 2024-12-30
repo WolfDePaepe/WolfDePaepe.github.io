213983fa2008fe700248173b7ffb01a3ada1f786
WolfDePaepe <wolf.de.paepe@gmail.com>
2024-12-30 12:28:30 +0100

    Utils.js

---
+++ Scripts/Utils.js
@@ -0,0 +1,31 @@
+export function _(query)
+{
+    let output = document.querySelectorAll(query);
+
+    if(query.charAt(0) == '#')
+    {
+        return [...output][0]
+    }
+    else
+    {
+        return [...output]
+    }
+}
+
+export function _new(type)
+{
+    return document.createElement(type);
+}
+
+export function _addClass(htmlElement, className)
+{
+    htmlElement.classList.add(className);
+}
+
+export function _setaAtributes(htmlElement ,attrObjArray)
+{
+    for(var key in attrObjArray)
+    {
+        htmlElement.setAttribute(key.attribute, key.attributeValue);
+    }
+}
